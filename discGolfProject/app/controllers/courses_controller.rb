5d9201744de919705a1a7c90b49a9f10e8962023
Hollyw00d <matt@mattjennings.net>
2015-08-27 22:25:39 -0700

    for single course view, google maps working and dealing with other stuff

--- discGolfProject/app/controllers/courses_controller.rb
+++ discGolfProject/app/controllers/courses_controller.rb
@@ -1,52 +1,35 @@
 class CoursesController < ApplicationController
   def index
-    if session[:user_id]
-      @user = User.find(session[:user_id])
-    end
     @courses = Course.all
   end
 
   def show
-    if session[:user_id]
-      @user = User.find(session[:user_id])
-    end
   	@course = Course.find(params[:id])
   end
 
-  def new
-    if session[:user_id]
-      @user = User.find(session[:user_id])
-    else
-      redirect_to '/'
-    end
-  end
-
   def create
-  	@course = Course.create(course_params)
+  	@course = Course.new(course_params)
   	redirect_to "/courses/#{@course.id}"
   end
 
   def edit
-    if session[:user_id]
-      @user = User.find(session[:user_id])
-    end
-    @course = Course.find(params[:id])
+    @courseEdit = Course.find(params[:id])
   end
 
   def update
-    @course = Course.find(params[:id])
-    @course.update(course_params)
+    @courseUpdate = Course.find(params[:id])
+    @courseUpdate.save
     redirect_to "/courses/#{@course.id}"
   end
 
   def delete
-    @course = Course.find(params[:id])
-    @course.destroy
+    course = Course.find(params[:id])
+    course.destroy
     redirect_to "/"
   end
 
   private
   	def course_params
-  		params.require(:course).permit(:name,:street,:city,:state,:zip_code,:google_map)
+  		params.require(:course).permit(:name,:street,:state,:zip_code,:google_map)
   	end
 end
